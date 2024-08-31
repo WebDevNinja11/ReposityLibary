efaee6661dd5aa219519cf64643baf97327182a7
The Syob <michnalex3@gmail.com>
2024-08-30 23:51:00 -0400

    upd

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
- document.getElementById("repository1").onclick = function(){
-    window.location.href = "https://WebDevNinja11.github.io/Clock"
- }
-document.getElementById("repository2").onclick = function(){
-    window.location.href = "https://webdevninja11.github.io/Website"
-}
+setTimeout(function(){
+    document.getElementById("repository1").onclick = function(){
+        window.location.href = "https://WebDevNinja11.github.io/Clock"
+     }
+    document.getElementById("repository2").onclick = function(){
+        window.location.href = "https://webdevninja11.github.io/Website"
+    }
+},100)
