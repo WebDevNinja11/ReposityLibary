d123983b5dc97e1e899a2dc3f7ee3d9e37dc6f24
The Syob <michnalex3@gmail.com>
2024-08-30 23:50:22 -0400

    upd

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
  document.getElementById("repository1").onclick = function(){
     window.location.href = "https://WebDevNinja11.github.io/Clock"
  }
+document.getElementById("repository2").onclick = function(){
+    window.location.href = "https://webdevninja11.github.io/Website"
+}
